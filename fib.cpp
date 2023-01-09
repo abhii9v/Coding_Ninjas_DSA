d65be8f8f42d78a1b2a44842c1159be7bb4bb831
Abhinav Yadav <abhi9v2204@gmail.com>
2023-01-09 14:40:45 +0530

    Initial Commit

--- fib.cpp
+++ fib.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int a = 1;
-    int b = 1;
-    int sum,n = 0;
-    cin >> n;
-    for (int i = 0; i < n - 2; i++)
-    {
-        sum = a + b;
-        a = b;
-        b = sum;
-    }
-    cout << sum;
+int square(int a){
+    int ans = a * a;
+    return ans;
+}
+
+int main() {
+    int a = 4;
+    a = square(a);
+    cout << a;
 }
